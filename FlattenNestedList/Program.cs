f2b24e39027783c40dcb1aa94ccd3b5f75663822
litepost <litepost@gmail.com>
2024-04-05 17:02:44 -0700

    decided not to do this problem

--- FlattenNestedList/Program.cs
+++ FlattenNestedList/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://leetcode.com/problems/flatten-nested-list-iterator/description/
+
+List<int> ints1 = [1,1];
+List<object> input = [ints1, 2, ints1];
+List<int> answer = [];
+
+
+static List<int> FlattenObject(object input, List<int> flattened) {
+    List<int> output = new List<int>();
+
+}
